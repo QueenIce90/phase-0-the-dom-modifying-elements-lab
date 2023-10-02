3e1b5e2e63b672667a316ef0f14cfcf02c5373c7
QueenIce90 <Isamargonzalez90@gmail.com>
2023-10-02 03:48:39 -0400

    short message briefly explaining the changes saved in this commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code here!
+// Create a new h1 element
+const newHeader = document.createElement("h1");
+
+// Set the id attribute of the newHeader element to 'victory'
+newHeader.id = "victory";
+
+// Set the text content of the newHeader element
+newHeader.textContent = "YOUR-NAME is the champion"; // Replace YOUR-NAME with the actual name
+
+// Find the main element by its id
+const mainElement = document.getElementById("main");
+
+// Remove the main element from the DOM
+mainElement.remove();
+
+
+
+
